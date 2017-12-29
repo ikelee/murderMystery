bc72fe6632463bb64e5ab3065845f2318af7cff2
ike lee <ikelee@ikes-MacBook-Pro.local>
2017-12-29 10:13:36 -0500

    App started

---
+++ murderMystery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  murderMystery
+//
+//  Created by ike lee on 2017-12-28.
+//  Copyright © 2017 Ike Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
